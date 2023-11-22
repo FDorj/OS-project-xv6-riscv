6a5254118fd1acdb32c3ca51f4cd973fb553d5f3
FDorj <f.dorj2@gmail.com>
2023-11-22 09:24:19 +0330

    history system call implemented

--- phase1/kernel/console.c
+++ phase1/kernel/console.c
@@ -21,6 +21,7 @@
 #include "riscv.h"
 #include "defs.h"
 #include "proc.h"
+#include "console.h"
 
 #define BACKSPACE 0x100
 #define C(x)  ((x)-'@')  // Control-x
@@ -51,6 +52,20 @@ struct {
   uint w;  // Write index
   uint e;  // Edit index
 } cons;
+
+struct {
+  char bufferArr[MAX_HISTORY][INPUT_BUF_SIZE];
+  uint lenghtsArr[MAX_HISTORY];
+  uint lastCommandIndex;
+  int numOfCommandsInMem;
+  int currentHistory;
+}historyBufferArray;
+
+struct {
+  int arrowIndex;
+  int countUp;
+  int countDown;
+}arrowUpDown;
 
 //
 // user write()s to the console go here.
@@ -135,9 +150,23 @@ consoleread(int user_dst, uint64 dst, int n)
 void
 consoleintr(int c)
 {
+  if (c == '[' && arrowUpDown.arrowIndex == 1) {
+    arrowUpDown.arrowIndex = 2;
+    return;
+  } else if ((c == 'A' || c == 'B') && arrowUpDown.arrowIndex == 2)
+  {
+    arrowKey(c == 'A');
+    return;
+  }
+  
+  
   acquire(&cons.lock);
 
   switch(c){
+    case '\e': 
+
+  arrowUpDown.arrowIndex = 1;
+  break;
   case C('P'):  // Print process list.
     procdump();
     break;
@@ -158,14 +187,40 @@ consoleintr(int c)
   default:
     if(c != 0 && cons.e-cons.r < INPUT_BUF_SIZE){
       c = (c == '\r') ? '\n' : c;
-
       // echo back to the user.
       consputc(c);
-
       // store for consumption by consoleread().
       cons.buf[cons.e++ % INPUT_BUF_SIZE] = c;
-
       if(c == '\n' || c == C('D') || cons.e-cons.r == INPUT_BUF_SIZE){
+        char historyInArray[7] = "history";
+        char commandString[INPUT_BUF_SIZE];
+        int j = 0;
+        int i = 0;
+        for(i = cons.w; i < cons.e; i++){ //copy command in commandString till before \n
+          commandString[j] = cons.buf[i];
+          j++;
+        }
+        commandString[j] = 0;
+        if(strncmp(commandString, historyInArray, 7) != 0){  // check if the command is not history
+          // Assuming commandString is a string and we want to store each character in the buffer
+          for(int i = 0; i < strlen(commandString); ++i) {
+            historyBufferArray.bufferArr[historyBufferArray.lastCommandIndex][i] = commandString[i];
+          }
+          historyBufferArray.lenghtsArr[historyBufferArray.lastCommandIndex] = strlen(commandString);
+          historyBufferArray.lastCommandIndex++;
+          historyBufferArray.currentHistory = historyBufferArray.lastCommandIndex -1;
+
+          if(historyBufferArray.lastCommandIndex == 16){
+            historyBufferArray.lastCommandIndex = 0;
+
+          }
+          
+          if(historyBufferArray.numOfCommandsInMem < 16){
+            historyBufferArray.numOfCommandsInMem++;
+          }
+
+          arrowUpDown.countUp = 0;
+        }
         // wake up consoleread() if a whole line (or end-of-file)
         // has arrived.
         cons.w = cons.e;
@@ -189,4 +244,63 @@ consoleinit(void)
   // to consoleread and consolewrite.
   devsw[CONSOLE].read = consoleread;
   devsw[CONSOLE].write = consolewrite;
+}
+
+int history(int historyId){
+  if(historyId < 16){
+      for(int i = 0; i < historyBufferArray.numOfCommandsInMem; i++){
+      printf("%s", historyBufferArray.bufferArr[i]);
+      }
+      printf("requested command: %s\n", historyBufferArray.bufferArr[historyId]);
+  }else{
+    return -1;
+  }
+  return 0;
+}
+
+int arrowKey(int isUp){
+  char commandToPrint[INPUT_BUF_SIZE];
+  while (cons.w != cons.e && cons.buf[(cons.e - 1) % INPUT_BUF_SIZE] != '\n'){
+        cons.e--;
+    consputc(BACKSPACE);
+  }
+
+  if (isUp){
+    // printf("UP");
+    if(historyBufferArray.numOfCommandsInMem > 0){
+      if((historyBufferArray.numOfCommandsInMem < 16 && historyBufferArray.currentHistory > -1) || (historyBufferArray.numOfCommandsInMem == 16 && arrowUpDown.countUp < historyBufferArray.numOfCommandsInMem)){
+
+      strncpy(commandToPrint, historyBufferArray.bufferArr[historyBufferArray.currentHistory], historyBufferArray.lenghtsArr[historyBufferArray.currentHistory]-1);
+      
+      for (int i = 0; i < INPUT_BUF_SIZE && commandToPrint[i] != 0; i++)
+      {
+        consoleintr(commandToPrint[i]);
+      }
+      arrowUpDown.countUp++;
+      arrowUpDown.countDown = arrowUpDown.countUp;
+      historyBufferArray.currentHistory--;
+      if(historyBufferArray.numOfCommandsInMem == 16 && historyBufferArray.currentHistory < 0){
+        historyBufferArray.currentHistory = 15;
+      }
+    return 0;
+      }
+    }
+  }else {
+    if(arrowUpDown.countDown > 0){
+    // printf("down");
+    strncpy(commandToPrint, historyBufferArray.bufferArr[historyBufferArray.currentHistory], historyBufferArray.lenghtsArr[historyBufferArray.currentHistory]-1);
+
+      for (int i = 0; i < INPUT_BUF_SIZE && commandToPrint[i] != 0; i++)
+      {
+        consoleintr(commandToPrint[i]);
+      }
+      arrowUpDown.countDown--;
+    historyBufferArray.currentHistory++;
+    if(historyBufferArray.numOfCommandsInMem == 16 && historyBufferArray.currentHistory > 15){
+      historyBufferArray.currentHistory = 0;
+    }
+    return 0;
+  }
+  }
+  return -1;
 }
